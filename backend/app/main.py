c120d3a4cf13e48f2b77919593d0e227756804bd
Abhinav Prasad Yasaswi <abhinavp@asu.edu>
2025-06-22 08:58:28 -0700

    Added some more CORS Urls

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,6 +16,7 @@ origins = [
     "http://127.0.0.1:5173",
     "https://your-frontend-app-name.onrender.com",
     "https://codequesters-heritage-square-founda.vercel.app/",
+    "https://codequesters-heritage-square-founda.vercel.app",
     "https://codequesters-heritage-square-founda-abhinav-usas-projects.vercel.app/",
     "https://codequesters-heritage-square-f-git-f0066e-abhinav-usas-projects.vercel.app/",
     "https://codequestors.vercel.app/",
